c60d40e71b8bea2b63a26a6da982f1e665febc7f
F0gboy <Jasp3r@live.dk>
2024-05-13 13:27:59 +0200

    en masse component stads

--- EksamensProjekt/EksamensProjekt/DesignPatterns/ComponentPattern/Component.cs
+++ EksamensProjekt/EksamensProjekt/DesignPatterns/ComponentPattern/Component.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,26 @@ using System.Threading.Tasks;
 
 namespace EksamensProjekt.DesignPatterns.ComponentPattern
 {
-    internal class Component
+    public abstract class Component
     {
-        public Component()
+        public Component(Game game)
         {
+            
+        }
 
+        public virtual void Initialize()
+        {
+            // Initialization logic goes here
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            // Update logic goes here
+        }
+
+        public virtual void Draw(GameTime gameTime)
+        {
+            // Draw logic goes here
         }
     }
 }
---
+++ EksamensProjekt/EksamensProjekt/DesignPatterns/ComponentPattern/SpriteRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EksamensProjekt.DesignPatterns.ComponentPattern
+{
+    public class SpriteRenderer : Component
+    {
+        private Texture2D texture;
+        private Vector2 position;
+
+        public SpriteRenderer(Game game, Texture2D texture, Vector2 position)
+            : base(game)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Update logic goes here
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+
+        public void SetPosition(Vector2 newPosition)
+        {
+            position = newPosition;
+        }
+    }
+}
