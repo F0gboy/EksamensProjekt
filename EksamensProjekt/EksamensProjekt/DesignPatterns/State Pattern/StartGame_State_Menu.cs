e0e2b69f165b949359ff47f18c6d6bf3617dafad
Marc Larsen <57370@student.eadania.dk>
2024-05-23 11:10:43 +0200

    flyttet state pattern ind i design pattern

---
+++ EksamensProjekt/EksamensProjekt/DesignPatterns/State Pattern/StartGame_State_Menu.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace EksamensProjekt.State_Pattern
+{
+    public class StartGame_State_Menu: I_State_Menu
+    {
+        private bool gameStart;
+
+        public bool GameStart
+        {
+            get { return gameStart; }
+        }
+
+        public void Update(Menu menu, GameTime gameTime)
+        {
+            MouseState mouseState = Mouse.GetState();
+            if (!menu.clicked && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                menu.clicked= true;
+               
+                    if (menu.startButton.Contains(mouseState.Position))
+                    {
+                        gameStart = true;
+                    }
+                
+            }
+                if( mouseState.LeftButton != ButtonState.Pressed )
+                {
+                    gameStart = true;
+                }
+        
+
+            if (mouseState.LeftButton != ButtonState.Pressed)
+            {
+                menu.clicked = false;
+            }
+        }
+
+        public void Draw(Menu menu, SpriteBatch spriteBatch)
+        {
+            if (!gameStart)
+            {
+                //spriteBatch.Draw(menu.background, Vector2.Zero, Color.White);
+                spriteBatch.Draw(menu.button, new Vector2(menu.startButton.Center.X - menu.button.Width, menu.startButton.Center.Y - menu.button.Height / 2), Color.White);
+                Vector2 registrationTextPosition = new Vector2(menu.startButton.Center.X - menu.font.MeasureString("Start").Length() - 45, menu.startButton.Center.Y - menu.font.MeasureString("Start").Y / 2);
+                spriteBatch.DrawString(menu.font, "Start", registrationTextPosition, Color.White);
+            }
+        }
+    }
+}
